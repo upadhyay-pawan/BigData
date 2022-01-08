92b1a0b90a3882d47cc7a6ac1d80147a45c17662
pawan <pawanjust4u2004@gmail.com>
2022-01-08 15:15:42 +0800

    new comps to add meta data to mysql

---
+++ DataPipeLine/src/main/java/com/datapipeline/MysqlConstants.java
@@ -0,0 +1,8 @@
+package com.datapipeline;
+
+public class MysqlConstants {
+    public static final String DRIVER_NAME = "com.mysql.cj.jdbc.Driver";
+    public static final String CONNECTION_URL = "jdbc:mysql://localhost:3306/";
+    public static final String USERNAME = "pavan";
+    public static final String PASSWORD = "P@ssw0rd2";
+}
